6a096fe0c939d892395de9bee3afd7ecba725b14
Jon Trowbridge <tro19008@byui.edu>
2021-05-13 08:42:34 -0400

    Team Activity 02 committed

--- routes/ta02.js
+++ routes/ta02.js
@@ -1,13 +1,48 @@
 //TA02 PLACEHOLDER
 // Remember, you can make more of these placeholders yourself! 
+const e = require('express');
 const express = require('express');
 const router = express.Router();
 
+const userArray = ['Jon', 'Heather', 'Oliver', 'Lex', 'Savannah'];
+var exists = true;
+var dup = false;
+
+router.post('/addUser',(req, res, next) => {
+    exists = true;
+    dup = false;
+    const name = req.body.username;
+    const index = userArray.indexOf(name);
+    if(index == -1) {
+        userArray.push(name);
+    } else {
+        dup = true;
+    };    
+    res.redirect('/ta02');
+});
+
+router.post('/removeUser',(req, res, next) => {
+    exists = true;
+    dup = false;
+    const name = req.body.removal;
+    const index = userArray.indexOf(name);
+    if(index !== -1) {
+        userArray.splice(index, 1);
+    } else {
+        exists = false;
+    };
+    
+    res.redirect('/ta02');
+});
+
 router.get('/',(req, res, next) => {
-    res.render('pages', { 
-        title: 'Team Activity 02', 
+    res.render('pages/ta02', { 
+        title: 'Team Activity 02',
+        users: userArray, 
+        exists: exists,
+        dup: dup,
         path: '/ta02', // For pug, EJS 
-        activeTA03: true, // For HBS
+        activeTA02: true, // For HBS
         contentCSS: true, // For HBS
     });
 });
